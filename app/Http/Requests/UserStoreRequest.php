d8166a747292756cb488a174bb7b92c6e39effda
victor <torvicv@gmail.com>
2024-04-16 17:55:06 +0200

    añadido password_confirmation

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -25,6 +25,7 @@ class UserStoreRequest extends FormRequest
             'name' => ['required','string','max:255'],
             'email' => ['required','string', 'email','max:255', 'unique:users'],
             'password' => ['required','string','min:8', 'confirmed'],
+            'password_confirmation' => ['required','string', 'min:8'],
         ];
     }
 }
